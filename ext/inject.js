30452791a1b85ce907e53a20124a207bda5844e1
James Liu <jamesjhliu@gmail.com>
2018-01-13 22:48:59 -0800

    basically working injected script

--- ext/inject.js
+++ ext/inject.js
@@ -3,12 +3,29 @@ function getImages() {
 	console.log(imgElements);
 
 	imgElements.forEach(imgEl => {
-    chrome.runtime.sendMessage({method: 'postUrl', url: img.src});
+		
+		imgEl.dataset.imgfilterSrc = imgEl.src;
+		// debugger;
+		imgEl.src = placeholderUrl(imgEl.width, imgEl.height);
+		// console.log(imgEl.src);
+		imgEl.setAttribute('srcset', '');
+
+		chrome.runtime.sendMessage({method: 'postUrl', url: imgEl.dataset.imgfilterSrc}, res => {
+			console.log(res);
+			if (!res.block) {
+				imgEl.src = imgEl.dataset.imgfilterSrc;
+			} else {
+				imgEl.style.filter = "blur(50px)";
+				imgEl.src = imgEl.dataset.imgfilterSrc;
+			}
+		});
 	});
 
-}
+};
 
 getImages();
 
-
+function placeholderUrl(w, h) {
+	return `https://via.placeholder.com/${w}x${h}`;
+}
 
