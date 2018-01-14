88bd201d2c58a80dd7da8842ce99769d144c2737
Arabelle Hou <arabelle310@yahoo.ca>
2018-01-14 04:42:45 +0000

    Nudity checker

---
+++ ballserver/main.py
@@ -0,0 +1,12 @@
+import classify_nsfw
+import subprocess
+
+res = classify_nsfw.main("test.jpg")
+print(
+classify_nsfw.main("test.jpg")
+classify_nsfw.main("test.jpg")
+classify_nsfw.main("test.jpg")
+classify_nsfw.main("test.jpg")
+classify_nsfw.main("test.jpg")
+classify_nsfw.main("test.jpg")
+classify_nsfw.main("test.jpg")
