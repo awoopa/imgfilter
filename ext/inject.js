a2f6d4eefc494599d56e7f2dbf4c44b1c1513fcf
James Liu <jamesjhliu@gmail.com>
2018-01-14 07:55:16 -0800

    mostly working div replacement

--- ext/inject.js
+++ ext/inject.js
@@ -3,11 +3,33 @@ function getImages() {
 	console.log(imgElements);
 
 	imgElements.forEach(imgEl => {
+
+		// create a parent div and copy over attributes
+		const parentDiv = document.createElement('div');
+
+
+		if (imgEl.classList) { parentDiv.classList = imgEl.classList; }
+		if (imgEl.id) { parentDiv.id = imgEl.id; }
+		if (imgEl.getAttribute('style')) {parentDiv.setAttribute('style', imgEl.getAttribute('style'));}
 		
+		// don't overwite existing width/height styles
+		if (!parentDiv.style.width) {parentDiv.style.width = imgEl.clientWidth = 'px'}
+		if (!parentDiv.style.height) {parentDiv.style.height = imgEl.clientHeight + 'px'}
+
+		// img elements are rendered as inline
+		parentDiv.style.display = 'inline';
+		
+		// insert div
+		imgEl.insertAdjacentElement('beforebegin', parentDiv);
+
+		//
+
+		parentDiv.appendChild(imgEl);
+
+
+
 		imgEl.dataset.imgfilterSrc = imgEl.src;
-		// debugger;
 		imgEl.src = placeholderUrl(imgEl.width, imgEl.height);
-		// console.log(imgEl.src);
 		imgEl.setAttribute('srcset', '');
 
 		chrome.runtime.sendMessage({method: 'postUrl', url: imgEl.dataset.imgfilterSrc}, res => {
@@ -15,7 +37,7 @@ function getImages() {
 			if (!res.block) {
 				imgEl.src = imgEl.dataset.imgfilterSrc;
 			} else {
-				imgEl.style.filter = "blur(50px)";
+				imgEl.style.filter = "blur(50px) brightness(0)";
 				imgEl.src = imgEl.dataset.imgfilterSrc;
 			}
 		});
